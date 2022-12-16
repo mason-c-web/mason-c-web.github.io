cbd012435f002b9247ce59a90cbfff1f05d926de
mason-c-web <79883350+mason-c-web@users.noreply.github.com>
2022-12-16 09:24:58 -0800

    removing broken files

--- seedsofgrace/donate.php
+++
@@ -1,7 +0,0 @@
-<?php include "includes/header.php"?>
-
-   <iframe width="100%" height="1600px" src='https://gateway.gravitylink.net/interface/epayform/PHEsuow11ZSQWZ543JrZS6j9vs8XR12J/' style='border:0'></iframe>
-
-
-
-<?php include "includes/footer.php"?>
