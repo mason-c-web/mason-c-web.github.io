9c8e2844798d879cc05d379386333fe8b18286a6
mason-c-web <79883350+mason-c-web@users.noreply.github.com>
2026-01-16 10:01:51 -0800

    Update VisitorCounter.tsx

--- app/ui/VisitorCounter.tsx
+++ app/ui/VisitorCounter.tsx
@@ -1,11 +1,11 @@
 export function VisitorCounter() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center ">
       <img
         style={{ paddingTop: "20px", paddingBottom: "10px" }}
         src="images/counter.png"
       />
-      <p>Visitors since May 11, 1999</p>
+      <p style={{ padding: 10 }}>Visitors since May 11, 1999</p>
     </div>
   );
 }
